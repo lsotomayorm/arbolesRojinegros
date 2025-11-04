70c06eb8960d9bea00b6bb5a77ede2e0a1e19886
Alfredo Paz Valderrama <apazv@unsa.edu.pe>
2025-11-04 16:23:19 -0500

    RedBlack corregido

--- RedBlackTree.h
+++ RedBlackTree.h
@@ -1,110 +1,115 @@
 #include <iostream>
-#include <algorithm> // Para std::max
 #include <string>
 
+enum Color { RED, BLACK };
+
 template <typename T>
-struct NodeAVL{
+struct NodeRBT{
   T key;
-  NodeAVL *left;
-  NodeAVL *right;
-  int height;
+  NodeRBT *parent;
+  NodeRBT *left;
+  NodeRBT *right;
+  Color color;
 };
 
 template <typename T>
-class AVLTree{
+class RedBlackTree{
 private:
-  NodeAVL *root;
+  NodeRBT<T> *root;
+  NodeRBT<T> *NIL; // Centinela
 
-  int height(NodeAVL<T> *N){
-    if(!N) // Modificado
-      return 0;
-    return N->height;
-  }
-
-  NodeAVL<T> *newNode(T key){
-    NodeAVL<T> *node = new NodeAVL<T>();
-    node->key = key;
-    node->left = nullptr;
-    node->right = nullptr;
-    node->height = 1; 
-    return (node);
-  }
-
-  NodeAVL<T> *rightRotate(NodeAVL<T> *y){
-    NodeAVL<T> *x = y->left;
-    NodeAVL<T> *T2 = x->right;
-
-    x->right = y;
-    y->left = T2;
-
-    y->height = std::max(height(y->left), height(y->right)) + 1;
-    x->height = std::max(height(x->left), height(x->right)) + 1;
-
-    return x;
-  }
-
-  NodeAVL<T> *leftRotate(NodeAVL<T> *x){
-    NodeAVL<T> *y = x->right;
-    NodeAVL<T> *T2 = y->left;
-
-    y->left = x;
-    x->right = T2;
-
-    x->height = std::max(height(x->left), height(x->right)) + 1;
-    y->height = std::max(height(y->left), height(y->right)) + 1;
-
-    return y;
-  }
-
-  int getBalance(NodeAVL<T> *N){
-    if(!N) // Modificado
-      return 0;
-    return height(N->left) - height(N->right);
-  }
-
-  NodeAVL<T> *insert(NodeAVL<T> *node, T key){
-    if(!node) // Modificado
-      return (newNode(key));
-
-    if(key < node->key)
-      node->left = insert(node->left, key);
-    else if(key > node->key)
-      node->right = insert(node->right, key);
-    else
-      return node;
-
-    node->height = 1 + std::max(height(node->left), height(node->right));
-
-    int balance = getBalance(node);
-
-    // Caso LL
-    if(balance > 1 && key < node->left->key)
-      return rightRotate(node);
-
-    // Caso RR
-    if(balance < -1 && key > node->right->key)
-      return leftRotate(node);
-
-    // Caso LR
-    if(balance > 1 && key > node->left->key){
-      node->left = leftRotate(node->left);
-      return rightRotate(node);
-    }
-
-    // Caso RL
-    if(balance < -1 && key < node->right->key){
-      node->right = rightRotate(node->right);
-      return leftRotate(node);
-    }
-
-    return node;
-  }
-
-  bool search(NodeAVL<T> *node, T key) const {
-    if(!node) // Modificado
-      return false;
+  void leftRotate(NodeRBT<T> *x){
+    NodeRBT<T> *y = x->right;
+    x->right = y->left;
     
-    if(node->key == key)
+    if(y->left != NIL){
+      y->left->parent = x;
+    }
+    
+    y->parent = x->parent;
+    
+    if(!x->parent) // Modificado
+      this->root = y;
+    else if(x == x->parent->left)
+      x->parent->left = y;
+    else
+      x->parent->right = y;
+    
+    y->left = x;
+    x->parent = y;
+  }
+
+  void rightRotate(NodeRBT<T> *x){
+    NodeRBT<T> *y = x->left;
+    x->left = y->right;
+    
+    if(y->right != NIL){
+      y->right->parent = x;
+    }
+    
+    y->parent = x->parent;
+    
+    if(!x->parent) // Modificado
+      this->root = y;
+    else if(x == x->parent->right)
+      x->parent->right = y;
+    else
+      x->parent->left = y;
+    
+    y->right = x;
+    x->parent = y;
+  }
+
+  void insertFixup(NodeRBT<T> *k){
+    NodeRBT<T> *u; 
+    
+    while(k->parent->color == RED){
+      if(k->parent == k->parent->parent->right){ 
+        u = k->parent->parent->left; 
+        
+        if(u->color == RED){
+          u->color = BLACK;
+          k->parent->color = BLACK;
+          k->parent->parent->color = RED;
+          k = k->parent->parent;
+        } else {
+          if(k == k->parent->left){
+            k = k->parent;
+            rightRotate(k);
+          }
+          k->parent->color = BLACK;
+          k->parent->parent->color = RED;
+          leftRotate(k->parent->parent);
+        }
+      } else {
+        u = k->parent->parent->right; 
+
+        if(u->color == RED){
+          u->color = BLACK;
+          k->parent->color = BLACK;
+          k->parent->parent->color = RED;
+          k = k->parent->parent;
+        } else {
+          if(k == k->parent->right){
+            k = k->parent;
+            leftRotate(k);
+          }
+          k->parent->color = BLACK;
+          k->parent->parent->color = RED;
+          rightRotate(k->parent->parent);
+        }
+      }
+      if(k == root)
+        break;
+    }
+    root->color = BLACK;
+  }
+
+  bool search(NodeRBT<T> *node, T key) const {
+    if(node == NIL) // Debe ser NIL
+      return false;
+
+    if(key == node->key)
       return true;
     
     if(key < node->key)
@@ -113,8 +118,8 @@ private:
       return search(node->right, key);
   }
 
-  void freeTree(NodeAVL<T> *node){
-    if(node){ // Modificado
+  void freeTree(NodeRBT<T> *node){
+    if(node != NIL){ // Debe ser NIL
       freeTree(node->left);
       freeTree(node->right);
       delete node;
@@ -122,17 +127,59 @@ private:
   }
 
 public:
-  AVLTree() : root(nullptr) {}
-  
-  ~AVLTree(){ 
-    freeTree(root); 
+  RedBlackTree(){
+    NIL = new NodeRBT<T>;
+    NIL->color = BLACK;
+    NIL->left = nullptr;
+    NIL->right = nullptr;
+    NIL->parent = nullptr;
+    root = NIL;
+  }
+
+  ~RedBlackTree(){
+    freeTree(this->root);
+    delete NIL;
   }
 
   void insert(T key){
-    root = insert(root, key);
+    NodeRBT<T> *node = new NodeRBT<T>;
+    node->key = key;
+    node->left = NIL;
+    node->right = NIL;
+    node->parent = nullptr;
+    node->color = RED; 
+
+    NodeRBT<T> *y = nullptr;
+    NodeRBT<T> *x = this->root;
+
+    while(x != NIL){ // Debe ser NIL
+      y = x;
+      if(node->key < x->key)
+        x = x->left;
+      else
+        x = x->right;
+    }
+
+    node->parent = y;
+    if(!y) // Modificado
+      root = node;
+    else if(node->key < y->key)
+      y->left = node;
+    else
+      y->right = node;
+
+    if(!node->parent){ // Modificado
+      node->color = BLACK;
+      return;
+    }
+
+    if(!node->parent->parent) // Modificado
+      return;
+
+    insertFixup(node);
   }
 
   bool search(T key) const {
-    return search(root, key);
+    return search(this->root, key);
   }
 };
